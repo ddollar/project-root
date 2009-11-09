128a63d16bc92141cc17e6dbf8443504496228cb
David Dollar <ddollar@gmail.com>
2009-11-08 22:23:38 -0500

    add vendor

--- lib/libdir.rb
+++ lib/libdir.rb
@@ -2,3 +2,8 @@ def libdir(path)
   relative_to = caller.first.split(':').first
   $:.unshift(File.join(File.dirname(relative_to), path))
 end
+
+def vendor(path)
+  relative_to = caller.first.split(':').first
+  $:.unshift(File.join(File.dirname(relative_to), 'vendor', path))
+end
